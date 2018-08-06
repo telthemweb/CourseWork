8ea2da775c30eb6cec343e4742cbac4670a0e0ad
Innocent Tauzeni <telthem.kerlyeen@gmail.com>
2018-08-06 14:48:38 -0700

    Course Work Calculator

---
+++ Coursework1/Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtAssignment1 = New System.Windows.Forms.TextBox()
+        Me.txtTest2 = New System.Windows.Forms.TextBox()
+        Me.txtTest1 = New System.Windows.Forms.TextBox()
+        Me.txtAssignment2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtExam = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtOveral = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtGrade = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtAssignment1
+        '
+        Me.txtAssignment1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAssignment1.Location = New System.Drawing.Point(247, 45)
+        Me.txtAssignment1.Name = "txtAssignment1"
+        Me.txtAssignment1.Size = New System.Drawing.Size(291, 35)
+        Me.txtAssignment1.TabIndex = 0
+        Me.txtAssignment1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtTest2
+        '
+        Me.txtTest2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTest2.Location = New System.Drawing.Point(247, 216)
+        Me.txtTest2.Name = "txtTest2"
+        Me.txtTest2.Size = New System.Drawing.Size(291, 35)
+        Me.txtTest2.TabIndex = 1
+        Me.txtTest2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtTest1
+        '
+        Me.txtTest1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTest1.Location = New System.Drawing.Point(247, 160)
+        Me.txtTest1.Name = "txtTest1"
+        Me.txtTest1.Size = New System.Drawing.Size(291, 35)
+        Me.txtTest1.TabIndex = 2
+        Me.txtTest1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtAssignment2
+        '
+        Me.txtAssignment2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAssignment2.Location = New System.Drawing.Point(247, 106)
+        Me.txtAssignment2.Name = "txtAssignment2"
+        Me.txtAssignment2.Size = New System.Drawing.Size(291, 35)
+        Me.txtAssignment2.TabIndex = 3
+        Me.txtAssignment2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(91, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 24)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Assignment 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(91, 113)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(136, 24)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Assignment 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(91, 164)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(148, 24)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "In-Class-Test 1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(91, 220)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(148, 24)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "In-Class-Test 2"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(91, 282)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(115, 24)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Final Exam"
+        '
+        'txtExam
+        '
+        Me.txtExam.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtExam.Location = New System.Drawing.Point(247, 278)
+        Me.txtExam.Name = "txtExam"
+        Me.txtExam.Size = New System.Drawing.Size(291, 35)
+        Me.txtExam.TabIndex = 8
+        Me.txtExam.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(91, 343)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(122, 24)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Overal Mark"
+        '
+        'txtOveral
+        '
+        Me.txtOveral.Enabled = False
+        Me.txtOveral.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOveral.Location = New System.Drawing.Point(247, 339)
+        Me.txtOveral.Name = "txtOveral"
+        Me.txtOveral.Size = New System.Drawing.Size(291, 35)
+        Me.txtOveral.TabIndex = 10
+        Me.txtOveral.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(91, 393)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(67, 24)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Grade"
+        '
+        'txtGrade
+        '
+        Me.txtGrade.Enabled = False
+        Me.txtGrade.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtGrade.Location = New System.Drawing.Point(247, 389)
+        Me.txtGrade.Name = "txtGrade"
+        Me.txtGrade.Size = New System.Drawing.Size(291, 35)
+        Me.txtGrade.TabIndex = 12
+        Me.txtGrade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Green
+        Me.Button1.Location = New System.Drawing.Point(190, 459)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 43)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Display"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Maroon
+        Me.Button2.Location = New System.Drawing.Point(415, 459)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(150, 43)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(709, 528)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtGrade)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtOveral)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtExam)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtAssignment2)
+        Me.Controls.Add(Me.txtTest1)
+        Me.Controls.Add(Me.txtTest2)
+        Me.Controls.Add(Me.txtAssignment1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Coursework"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtAssignment1 As TextBox
+    Friend WithEvents txtTest2 As TextBox
+    Friend WithEvents txtTest1 As TextBox
+    Friend WithEvents txtAssignment2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtExam As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtOveral As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtGrade As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Coursework1/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles txtAssignment2.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'a1 + a2 + t1+ t2 constitute 30% of the final exam
+        'exam take 70% 
+        Dim Assignment1 As Integer = txtAssignment1.Text
+        Dim Assignment2 As Integer = txtAssignment2.Text
+        Dim T1 As Integer = txtTest1.Text
+        Dim T2 As Integer = txtTest2.Text
+        Dim exam As Double = txtExam.Text
+        Dim examf As Double = (exam / 100) * 70
+        Dim TotalAT As Double
+        Dim overalMark As Double
+
+        'calculating total
+        TotalAT = (Assignment1 + Assignment2) + (T1 + T2)
+
+        Dim totalCoursework As Double
+        totalCoursework = (TotalAT / 400) * 30
+
+        'MsgBox(totalCoursework)
+
+        'Final mark after adding coursework and final exam 30% + 70% = 100%
+        overalMark = totalCoursework + examf
+        txtOveral.Text = overalMark
+
+
+
+        If overalMark >= 75 And overalMark <= 100 Then
+            txtGrade.Text = "Distinction"
+
+        ElseIf overalMark >= 70 And overalMark <= 74 Then
+            txtGrade.Text = "Merit"
+        ElseIf overalMark >= 60 And overalMark <= 69 Then
+            txtGrade.Text = "Credit"
+
+        ElseIf overalMark >= 50 And overalMark <= 59 Then
+            txtGrade.Text = "Pass"
+        Else
+
+            txtGrade.Text = "F"
+        End If
+
+
+
+
+
+
+
+    End Sub
+End Class
